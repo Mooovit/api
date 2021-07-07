dbc8b1306a7fd8493c370216fe79e08606a58340
Matthieu TIRELLI <matthieu@wisembly.com>
2021-07-07 08:45:06 +0200

    Update models relations

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -11,8 +11,13 @@ class Item extends Model
     use HasFactory;
     use Uuids;
 
-    public function box()
-    {
-        return $this->belongsTo(Box::class);
+    public $fillable = ['name', 'team_id', 'location_id', 'status_id', 'parent_id'];
+
+    public function childrens() {
+        return $this->hasMany(Item::class, "parent_id");
+    }
+
+    public function parent() {
+        return $this->belongsTo(Item::class);
     }
 }
