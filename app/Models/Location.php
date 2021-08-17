b5078507f9401b531726b85e02c42093705f2136
Matthieu TIRELLI <matthieu@wisembly.com>
2021-08-17 22:28:34 +0200

    Fix controllers and add phpdoc (#14)
    Co-authored-by: Travis Build <build@travis.com>

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -5,11 +5,25 @@ namespace App\Models;
 use App\Traits\Uuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Location extends Model
 {
     public $fillable = ['name', 'team_id'];
+    /**
+     * @var mixed
+     */
+    public $team;
 
     use HasFactory;
     use Uuids;
+
+    /**
+     * Team Relation
+     * @return BelongsTo
+     */
+    public function team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class);
+    }
 }
